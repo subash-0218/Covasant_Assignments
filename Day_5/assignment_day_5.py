a198294f9a657592d632f24badb1e604ac679af8
Kanchamreddy Venkata Subash Reddy <126976302+subash-0218@users.noreply.github.com>
2025-04-12 20:41:15 +0530

    Update assignment_day_5.py

--- Day_5/assignment_day_5.py
+++ Day_5/assignment_day_5.py
@@ -7,36 +7,41 @@
 import requests
 from bs4 import BeautifulSoup
 from concurrent.futures import ThreadPoolExecutor
+from urllib.parse import urljoin
 
-def download_page(url): 
+def download_page(url):
+    
     response = requests.get(url)
+    
     return response
     
-def parse_page_links(response):
+def parse_page_links(response,base_url):
+    
     soup = BeautifulSoup(response.text,"html.parser")
+    
     links = set()
     
     for a_tag in soup.find_all("a", href=True):
-        link = a_tag["href"]
-        if link.startswith("http"):
-            links.add(link)
-            
+        link = urljoin(base_url,a_tag["href"])
+        links.add(link)
+        
     return links
     
 def download_all(url):
-    current_url = [url]
+    
+    html = download_page(url)
+    
+    links = parse_page_links(html,url)
+    
+    print(f"Links found in {url}:\n")
+    for link in links:
+        print(link)
+    
     with ThreadPoolExecutor(max_workers = 5) as executor:
-        while current_url:
-            results = list(executor.map(download_page , current_url))
-            current_url = []
-            for html in results:
-                if html :
-                    links = parse_download_links(html)
-                    print("Extracted Links:",links)
-                    current_url.extend(links)
+        executor.map(download_page ,links)
+
         
     
-if __name__ == "__main__":
-  
+if __name__ == "__main__":   
     url = "https://www.google.co.in/?gws_rd=ssl"
     download_all(url)
