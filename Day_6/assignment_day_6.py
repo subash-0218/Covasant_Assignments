9980a4751b5035d346244dec1b66e801fb320545
Kanchamreddy Venkata Subash Reddy <126976302+subash-0218@users.noreply.github.com>
2025-04-13 22:00:12 +0530

    Python_assignment

--- Day_6/assignment_day_6.py
+++ Day_6/assignment_day_6.py
@@ -9,17 +9,16 @@ from bs4 import BeautifulSoup
 import asyncio
 import aiohttp
 from urllib.parse import urljoin
+import time
 
 
-async def download_page(session,url):
-    
-    async with session.get(url) as response:
-        return await response.text()
+async def download_page(url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            return await response.text()
     
 def parse_page_links(response,base_url):
-    
     soup = BeautifulSoup(response,"html.parser")
-    
     links = set()
     
     for a_tag in soup.find_all("a", href=True):
@@ -29,23 +28,27 @@ def parse_page_links(response,base_url):
     return links
     
     
-async def download_all(url):
+async def download_all(url):   
+    html = await download_page(url)   
+    links = parse_page_links(html,url) 
     
-    async with aiohttp.ClientSession() as session:
-        
-        html = await download_page(session,url)
-        
-        links = parse_page_links(html,url)
-        
-        print(f"Links found in {url}:\n")
-        for link in links:
-            print(link)
+    print(f"Links found in {url}:\n")
+    for link in links:
+        print(link)
             
-        tasks = [download_page(session,link) for link in links]
-        
-        await asyncio.gather(*tasks)
-        
-        
+    tasks = [asyncio.create_task(download_page(link)) for link in links]   
+    result = [await task for task in tasks]
+    
+    return result
+
+
 if __name__ == "__main__":
     url = "https://www.google.co.in/?gws_rd=ssl"
+    start_time = time.time()
     asyncio.run(download_all(url))
+    print("\n---Successfully downloaded the total content from given url ---")
+    print(f"\nTotal Time taken : {time.time() - start_time} seconds")
+
+
+#---Successfully downloaded the total content from given url ---
+#Total Time taken : 1.4462833404541016 second
